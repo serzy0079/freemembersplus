f9f85e073e425f432ae3e6dbdc629e0e33b06501
Serzy <149769387+serzy0079@users.noreply.github.com>
2023-11-04 07:36:02 +0100

    Add files via upload

---
+++ config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    bot: require("./bot.json"),
+    color: require("./color.json"),
+    supportGuild: "https://discord.gg/qSVnqyPqje",
+    unbanGuild: "",
+    imageBot: "https://media.discordapp.net/attachments/1167048211819675669/1168153578460946542/IMG_0115.png?ex=6550bacc&is=653e45cc&hm=fa4637d3197437febf270a7cb750a7c128f6350162ab243f16a8ead0a4b55f5a&",
+    ownersId: "758343011117170719"
+};
